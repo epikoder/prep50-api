83b90e357f7c07d42fd6f460b03455d191d6e119
epikoder <efedua.bell@gmail.com>
2022-06-10 17:19:34 +0100

    added routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,7 +1,13 @@
 package routes
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/Prep50mobileApp/prep50-api/src/controllers"
+	"github.com/kataras/iris/v12"
+)
 
 func RegisterApiRoutes(app *iris.Application) {
-
+	auth := app.Party("/auth")
+	auth.Post("/register", controllers.RegisterV1)
+	auth.Post("/login", controllers.LoginV1)
+	auth.Post("/social/{id}", controllers.SocialV1)
 }
