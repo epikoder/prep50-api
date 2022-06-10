ddf330fb42430344438cfb8e97f67ad3d0685b7e
epikoder <efedua.bell@gmail.com>
2022-06-10 17:18:03 +0100

    updated models for authentication

---
+++ src/models/provider.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"github.com/Prep50mobileApp/prep50-api/src/pkg/dbmodel"
+	"github.com/Prep50mobileApp/prep50-api/src/services/database"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type (
+	Provider struct {
+		Id   uuid.UUID `sql:"primary_key;index;type:uuid;default:uuid_generate_v4()" json:"-"`
+		Name string    `gorm:"type:varchar(50);column:name" json:"name"`
+	}
+)
+
+func (u *Provider) Database() *gorm.DB {
+	return database.UseDB("app")
+}
+
+func (p *Provider) Migrate() dbmodel.Migration {
+	return dbmodel.NewMigration(p)
+}
--- src/models/user.go
+++ src/models/user.go
@@ -1,33 +1,43 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/Prep50mobileApp/prep50-api/src/pkg/dbmodel"
+	"github.com/Prep50mobileApp/prep50-api/src/services/database"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type (
-	IUser interface {
-		ID() uuid.UUID
-		Name() string
-		UserName() string
-		GetEmail() string
+	User struct {
+		Id        uuid.UUID  `sql:"primary_key;index;type:uuid;default:uuid_generate_v4()" json:"-"`
+		UserName  string     `gorm:"type:varchar(50);column:username" json:"username"`
+		Email     string     `gorm:"type:varchar(50);column:email;unique;index;" json:"email"`
+		Phone     string     `gorm:"type:varchar(50);column:phone;unique;index;" json:"phone"`
+		Password  string     `gorm:"type:varchar(50);column:password" json:"password"`
+		Providers []Provider `gorm:"many2many:user_providers;"`
 	}
 
-	User struct {
-		Id    uuid.UUID
-		FName string
-		LName string
-		UName string
-		Email string
+	UserProvider struct {
+		Token string
+	}
+
+	UserRegisterFormStruct struct {
+		UserName string `validate:"required,string"`
+		Email    string `validate:"required,email"`
+		Phone    string `validate:"required,numeric,min=8"`
+		Password string `validate:"required,string"`
+	}
+
+	UserLoginFormStruct struct {
+		UName    string
+		Password string
 	}
 )
 
-func (u *User) ID() (id uuid.UUID) {
-	return
+func (u *User) Database() *gorm.DB {
+	return database.UseDB("app")
 }
-func (u *User) Name() (name string) {
-	return
-}
-func (u *User) UserName() (username string) {
-	return
-}
-func (u *User) GetEmail() (email string) {
-	return
+
+func (u *User) Migrate() dbmodel.Migration {
+	return dbmodel.NewMigration(u)
 }
