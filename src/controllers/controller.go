aab0cb936e4b7c7f34279525cf20e60f2980090b
epikoder <efedua.bell@gmail.com>
2022-09-27 06:51:23 +0100

    Refactor

--- src/controllers/controller.go
+++ src/controllers/controller.go
@@ -2,17 +2,12 @@ package controllers
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/Prep50mobileApp/prep50-api/src/models"
-	"github.com/Prep50mobileApp/prep50-api/src/pkg/hash"
 	"github.com/Prep50mobileApp/prep50-api/src/pkg/logger"
 	"github.com/Prep50mobileApp/prep50-api/src/pkg/repository"
-	"github.com/Prep50mobileApp/prep50-api/src/pkg/sendmail"
 	"github.com/Prep50mobileApp/prep50-api/src/pkg/settings"
-	"github.com/Prep50mobileApp/prep50-api/src/pkg/validation"
-	"github.com/Prep50mobileApp/prep50-api/src/services/queue"
 	"github.com/google/uuid"
 	"github.com/kataras/iris/v12"
 )
@@ -55,151 +50,6 @@ var (
 		return u, nil
 	}
 )
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++
-func PasswordReset(ctx iris.Context) {
-	type PasswordResetStruct struct {
-		Email string `validate:"required"`
-	}
-	prs := PasswordResetStruct{}
-	if err := ctx.ReadJSON(&prs); err != nil {
-		ctx.StatusCode(http.StatusBadRequest)
-		ctx.JSON(validation.Errors(err))
-		return
-	}
-	user := &models.User{}
-	{
-		if ok := repository.NewRepository(user).FindOne("email = ? OR username = ?", prs.Email, prs.Email); !ok {
-			ctx.StatusCode(http.StatusNotFound)
-			ctx.JSON(apiResponse{
-				"status":  "failed",
-				"message": "user not found",
-			})
-			return
-		}
-
-		if user.IsProvider {
-			ctx.StatusCode(http.StatusForbidden)
-			ctx.JSON(apiResponse{
-				"status":  "failed",
-				"code":    400,
-				"message": "password reset not available for this account",
-			})
-			return
-		}
-	}
-
-	queue.Dispatch(queue.Job{
-		Type: queue.SendMail,
-		Func: func() error {
-			return sendmail.SendPasswordResetMail(user)
-		},
-		Schedule: time.Now().Add(time.Second * 5),
-		Retries:  3,
-	})
-
-	ctx.JSON(apiResponse{
-		"status":  "success",
-		"message": "Use the code sent to your email address to complete your request",
-	})
-}
-
-func VerifyPasswordResetCode(ctx iris.Context) {
-	pr := &models.PasswordReset{}
-	if ok := repository.NewRepository(pr).FindOne("code = ? AND email = ?", ctx.URLParam("code"), ctx.URLParam("email")); !ok || time.Since(pr.CreatedAt).Minutes() > 30 {
-		ctx.StatusCode(http.StatusBadRequest)
-		ctx.JSON(apiResponse{
-			"status":  "failed",
-			"message": "expired or invalid code",
-		})
-		return
-	}
-	ctx.JSON(apiResponse{
-		"status": "success",
-	})
-}
-
-func CompletePasswordReset(ctx iris.Context) {
-	type CompletePasswordResetStruct struct {
-		Email    string `validate:"required"`
-		Code     string `validate:"required,numeric"`
-		Password string `validate:"required,alphanum"`
-	}
-
-	cprs := CompletePasswordResetStruct{}
-	if err := ctx.ReadJSON(&cprs); err != nil {
-		ctx.StatusCode(http.StatusBadRequest)
-		ctx.JSON(validation.Errors(err))
-		return
-	}
-
-	{
-		pr := &models.PasswordReset{}
-		if ok := repository.NewRepository(pr).FindOne("code = ? AND (email = ? OR username = ?)", cprs.Code, cprs.Email, cprs.Email); !ok || time.Since(pr.CreatedAt).Minutes() > 30 {
-			ctx.StatusCode(http.StatusBadRequest)
-			ctx.JSON(apiResponse{
-				"status":  "failed",
-				"message": "expired or invalid code",
-			})
-			return
-		}
-	}
-
-	user := &models.User{}
-	{
-		if ok := repository.NewRepository(user).FindOne("email = ?", cprs.Email); !ok {
-			ctx.StatusCode(http.StatusNotFound)
-			ctx.JSON(apiResponse{
-				"status":  "failed",
-				"message": "user does not exist",
-			})
-			return
-		}
-
-		if user.IsProvider {
-			ctx.StatusCode(http.StatusForbidden)
-			ctx.JSON(apiResponse{
-				"status":  "failed",
-				"code":    400,
-				"message": "password reset not available for this account",
-			})
-			return
-		}
-		if ok := hash.CheckHash(user.Password, cprs.Password); ok {
-			ctx.StatusCode(http.StatusForbidden)
-			ctx.JSON(apiResponse{
-				"status":  "failed",
-				"code":    401,
-				"message": "New password is same as old password",
-			})
-			return
-		}
-	}
-	p, err := hash.MakeHash(cprs.Password)
-	if err != nil {
-		ctx.StatusCode(http.StatusBadRequest)
-		ctx.JSON(apiResponse{
-			"status":  "failed",
-			"message": "error occcured",
-		})
-		return
-	}
-
-	user.Password = p
-	if err := repository.NewRepository(user).Save(); !logger.HandleError(err) {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(apiResponse{
-			"status":  "failed",
-			"message": "error occcured",
-		})
-		return
-	}
-	ctx.StatusCode(http.StatusOK)
-	ctx.JSON(apiResponse{
-		"status":  "success",
-		"message": "Password reset successful",
-	})
-}
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++
 
---
+++ src/controllers/password_reset_controller.go
@@ -0,0 +1,94 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/Prep50mobileApp/prep50-api/src/models"
+	"github.com/Prep50mobileApp/prep50-api/src/pkg/crypto"
+	"github.com/Prep50mobileApp/prep50-api/src/pkg/helper"
+	"github.com/Prep50mobileApp/prep50-api/src/pkg/repository"
+	"github.com/Prep50mobileApp/prep50-api/src/pkg/sendmail"
+	"github.com/Prep50mobileApp/prep50-api/src/services/queue"
+	"github.com/kataras/iris/v12"
+)
+
+type PasswordResetController struct {
+	Ctx iris.Context
+}
+
+func (c *PasswordResetController) Get() {
+	_token, err := crypto.Aes256Decode(c.Ctx.URLParam("token"))
+	if err == nil {
+		c.Ctx.View("password_reset", iris.Map{
+			"message": "Invalid/Expired Link",
+		})
+		return
+	}
+	token := sendmail.VerificationToken{}
+	if err := json.Unmarshal([]byte(_token), &token); err != nil {
+		c.Ctx.View("password_reset", iris.Map{
+			"message": "Invalid/Expired Link",
+		})
+		return
+	}
+
+	if token.Expires.Before(time.Now()) {
+		c.Ctx.View("password_reset", iris.Map{
+			"message": "Link has Expired",
+		})
+		return
+	}
+
+	user := &models.User{}
+	if ok := repository.NewRepository(user).FindOne("email = ?", token.Email); !ok {
+		return
+	}
+	c.Ctx.View("password_reset", iris.Map{
+		"message": "Create new password",
+		"user":    user,
+	})
+}
+
+type PasswordResetForm struct {
+	User string `validate:"required"`
+}
+
+func (c *PasswordResetController) Post(prs PasswordResetForm) {
+	user := &models.User{}
+	{
+		if ok := repository.NewRepository(user).FindOne("email = ? OR username = ?", prs.User, prs.User); !ok {
+			c.Ctx.StatusCode(http.StatusNotFound)
+			c.Ctx.JSON(apiResponse{
+				"status":  "failed",
+				"message": "user not found",
+			})
+			return
+		}
+
+		if user.IsProvider {
+			c.Ctx.StatusCode(http.StatusForbidden)
+			c.Ctx.JSON(apiResponse{
+				"status":  "failed",
+				"code":    400,
+				"message": "password reset not available for this account",
+			})
+			return
+		}
+	}
+
+	queue.Dispatch(queue.Job{
+		Type: queue.SendMail,
+		Func: func() error {
+			return sendmail.SendPasswordResetMail(user, helper.GetOrigin(c.Ctx))
+		},
+		Schedule: time.Now().Add(time.Second * 5),
+		Retries:  3,
+	})
+
+	c.Ctx.JSON(apiResponse{
+		"status":  "success",
+		"message": "We just sent you an email so you can create a new password for your account and get back to your fun studies.",
+	})
+}
